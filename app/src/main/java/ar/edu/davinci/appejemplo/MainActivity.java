12ce988e97c25dd614256ac881831bb7fbb17f25
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-10-04 22:46:18 -0300

    feat: update text background

--- app/src/main/java/ar/edu/davinci/appejemplo/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/appejemplo/MainActivity.java
@@ -3,6 +3,8 @@ package ar.edu.davinci.appejemplo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView helloWorldText = findViewById(R.id.helloWorldText);
+
+        helloWorldText.setText("Botón Cliqueado");
+        helloWorldText.setBackgroundColor(+R.color.pink_200);
     }
 }
